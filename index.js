dd95116136fd4be449a4b5788de1865e4c0d1864
ishratmalihaa <ishrat.malihaa@gmail.com>
2026-04-20 17:31:07 -1200

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+  
+  console.log('Verify attempt:', mode, token);
+  
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK VERIFIED');
+      return res.status(200).send(challenge);
+    }
   }
+  return res.sendStatus(403);
 });
 
 app.post('/webhook', (req, res) => {
-  const body = req.body;
-  console.log('Message received:', JSON.stringify(body));
+  console.log('Message received:', JSON.stringify(req.body));
   res.status(200).send('EVENT_RECEIVED');
+});
+
+app.get('/', (req, res) => {
+  res.send('BizAssist Webhook Server is running!');
 });
 
 const PORT = process.env.PORT || 3000;
